96b875a30dd153acf89ef3c655862b4455e0378b
zhewan <5289886+kobe_zzw@user.noreply.gitee.com>
2021-05-20 22:46:52 +0800

    分支新建文件

---
+++ restkeeper_service/restkeeper_operator/src/test/com/restkeeper/operator/option.java
@@ -0,0 +1,15 @@
+package com.restkeeper.operator;
+
+/**
+ * @program: RestKeeper
+ * @author: zhanzhewan
+ * @create: 2021-05-20 22:45
+ **/
+public class option {
+    public void go(){
+
+    }
+    public String bring(){
+        return "asf";
+    }
+}
