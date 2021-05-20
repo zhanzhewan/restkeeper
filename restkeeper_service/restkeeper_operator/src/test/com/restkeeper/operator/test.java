b173f28c7cf394dff8300b33b48a1ad1e7882db4
zhewan <5289886+kobe_zzw@user.noreply.gitee.com>
2021-05-20 22:43:43 +0800

    新建一个test文件

---
+++ restkeeper_service/restkeeper_operator/src/test/com/restkeeper/operator/test.java
@@ -0,0 +1,21 @@
+package com.restkeeper.operator;
+
+/**
+ * @program: RestKeeper
+ * @author: zhanzhewan
+ * @create: 2021-05-20 22:42
+ **/
+public class test {
+    public void send(){
+
+    }
+
+    public int eat(){
+        int i = 10;
+        return i;
+    }
+
+    public String sleep(){
+
+    }
+}
